fcd79d2f3a3e16d4ab8364ecbaa076791ce6b6e6
Chris Spradling <cjspradling@gmail.com>
2015-12-08 11:36:05 -0600

    finished standalone shiny modules

--- 04 Shiny (ui.R and server.R)/ChrisShiny/server.R
+++ 04 Shiny (ui.R and server.R)/ChrisShiny/server.R
@@ -9,45 +9,68 @@ require(shinydashboard)
 require(leaflet)
 require(DT)
 
+
+## ISLAM
 shinyServer(function(input, output) {
   religion_table <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query="""SELECT STATE_NAME, YEAR YR, ISLMGENPCT * 100 islam_total, ISLMSUNPCT * 100 sunni, ISLMSHIPCT * 100 shiite, ISLMOTHRPCT * 100 islam_other, ISLMNATPCT * 100 Nation_Of_Islam, ISLMIBDPCT * 100 Ibadhi, ISLMAHMPCT * 100 Ahmadiyya, ISLMALWPCT * 100 Alawite FROM RELIGIONS_BY_NATION LEFT JOIN COW_STATE_CODES ON COW_STATE_CODES.STATE_NUM = RELIGIONS_BY_NATION.STATE"""')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
   
-  output$distPlot1 <- renderPlot(height = 500, width = 1000, {   
-  
-  
-  country_selection <- input$selectCountry
-  
-    religion_table %>% filter(STATE_NAME == country_selection) %>% select(YR, ISLAM_OTHER, SUNNI, SHIITE, ALAWITE, AHMADIYYA, IBADHI, NATION_OF_ISLAM) -> religion_table_filtered
-    
+  output$islam_plot <- renderPlot(height = 500, width = 1000, {   
+    country_selection1 <- input$selectCountry1
+    religion_table %>% filter(STATE_NAME == country_selection1) %>% select(YR, ISLAM_OTHER, SUNNI, SHIITE, ALAWITE, AHMADIYYA, IBADHI, NATION_OF_ISLAM) -> religion_table_filtered
     melt(religion_table_filtered, id.vars = "YR", variable.name = "SECT", value.name = "PCT") -> religion_table_melted
-    
-  
-          
-    religion_plot <- ggplot() +
+    islam_plot <- ggplot() +
       layer(data=religion_table_melted, 
             mapping=aes(x=as.numeric(as.character(YR)),y=PCT, fill=SECT),
             stat="identity", 
             stat_params=list(),
             geom="area",
             geom_params=list()
-      ) + labs(title=country_selection) +
+      ) + labs(title=country_selection1) +
       labs(x="Year", y="% of Population") +
       guides(fill=guide_legend(title=NULL)) +
       expand_limits(y=c(0,100)) +
       expand_limits(x=c(1945,2015)) +
       scale_x_continuous(breaks=c(1945, 1950, 1955, 1960, 1965, 1970, 1975, 1980, 1985, 1990, 1995, 2000, 2005, 2010, 2015))
-
-    
-    
-    
-    return(religion_plot)
+    return(islam_plot)
   })
+  
+  
+  
+  ## BUDDHISM
+  buddh_table <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query="""SELECT STATE_NAME, YEAR YR, BUDGENPCT * 100 buddhist_total, BUDTHRPCT * 100 theravadan, BUDMAHPCT * 100 mahayan, BUDOTHRPCT * 100 buddhist_other FROM RELIGIONS_BY_NATION LEFT JOIN COW_STATE_CODES ON COW_STATE_CODES.STATE_NUM = RELIGIONS_BY_NATION.STATE"""')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+  
+  output$buddh_plot <- renderPlot(height = 500, width = 1000, {   
+    country_selection2 <- input$selectCountry2
+    buddh_table %>% filter(STATE_NAME == country_selection2) %>% select(YR, BUDDHIST_OTHER, THERAVADAN, MAHAYAN) -> buddh_table_filtered
+    melt(buddh_table_filtered, id.vars = "YR", variable.name = "SECT", value.name = "PCT") -> buddh_table_melted
+    buddh_plot <- ggplot() +
+      layer(data=buddh_table_melted, 
+            mapping=aes(x=as.numeric(as.character(YR)),y=PCT, fill=SECT),
+            stat="identity", 
+            stat_params=list(),
+            geom="area",
+            geom_params=list()
+      ) + labs(title=country_selection2) +
+      labs(x="Year", y="% of Population") +
+      guides(fill=guide_legend(title=NULL)) +
+      expand_limits(y=c(0,100)) +
+      expand_limits(x=c(1945,2015)) +
+      scale_x_continuous(breaks=c(1945, 1950, 1955, 1960, 1965, 1970, 1975, 1980, 1985, 1990, 1995, 2000, 2005, 2010, 2015))
+    
+    return(buddh_plot)
+  })
+  
+  
   
   
   
 })
   
   
+
+
+
+
 
 
 
--- 04 Shiny (ui.R and server.R)/ChrisShiny/ui.R
+++ 04 Shiny (ui.R and server.R)/ChrisShiny/ui.R
@@ -8,19 +8,26 @@ dashboardPage(
   ),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Crosstab", tabName = "crosstab", icon = icon("dashboard")),
+      menuItem("Crosstab", tabName = "Islam", icon = icon("dashboard")),
+      menuItem("Crosstab", tabName = "Buddhism", icon = icon("dashboard")),
       menuItem("Barchart", tabName = "barchart", icon = icon("th"))
     )
   ),
   dashboardBody(
     tabItems(
       # First tab content
-      tabItem(tabName = "crosstab",
-              selectInput("selectCountry", h3("Country"), choices = c("Barbados","Benin","Bulgaria","Burkina Faso","Cameroon","Central African Republic","Chad","Comoros","Cyprus","Djibouti","Eritrea","Ethiopia","Gambia","Ghana","Guinea","Guinea-Bissau","India","Indonesia","Iran","Iraq","Ivory Coast","Kenya","Kuwait","Kyrgyzstan","Liberia","Macedonia","Malawi","Malaysia","Maldives","Mali","Mauritania","Mauritius","Mozambique","Niger","Nigeria","Pakistan","Russia","Senegal","Sierra Leone","Somalia","Tanzania","Thailand","Togo","Uganda","United Arab Emirates","Yugoslavia"), selected = "Barbados"),
+      tabItem(tabName = "Buddhism",
+              selectInput("selectCountry2", h3("Country"), choices = c("Bhutan","Cambodia","Laos","Sri Lanka","Thailand","Vietnam"), selected = "Bhutan"),
               
-              plotOutput("distPlot1")
+              
+              plotOutput("buddh_plot")
       ),
-      
+      tabItem(tabName = "Islam",
+              selectInput("selectCountry1", h3("Country"), choices = c("Barbados","Benin","Bulgaria","Burkina Faso","Cameroon","Central African Republic","Chad","Comoros","Cyprus","Djibouti","Eritrea","Ethiopia","Gambia","Ghana","Guinea","Guinea-Bissau","India","Indonesia","Iran","Iraq","Ivory Coast","Kenya","Kuwait","Kyrgyzstan","Liberia","Macedonia","Malawi","Malaysia","Maldives","Mali","Mauritania","Mauritius","Mozambique","Niger","Nigeria","Pakistan","Russia","Senegal","Sierra Leone","Somalia","Tanzania","Thailand","Togo","Uganda","United Arab Emirates","Yugoslavia"), selected = "Barbados"),
+              
+              
+              plotOutput("islam_plot")
+      ),
       # Second tab content
       tabItem(tabName = "barchart",
               actionButton(inputId = "clicks2",  label = "Click me"),
